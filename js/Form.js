843925aafe81d39d37d473b0eb4cb241c15e6e04
vicwski <90584286+vicwski@users.noreply.github.com>
2022-09-30 16:52:51 -0300

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,16 @@
+class Form {
+  constructor() {
+    this.input = createInput('').attribute('placeholder', 'Digite seu nome')
+    this.playButton = createButton('Jogar')
+    this.titleImg = createImg('./assets/TITULO.png', 'nome do jogo')
+    this.greeting = createElement('h2')
+  }
+
+  display() {}
+
+  hide() {
+    this.greeting.hide()
+    this.playButton.hide()
+    this.input.hide()
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,25 @@
+var canvas
+var backgroundImage
+var bgImg
+var database
+var form, player
+var playerCount
+
+function preload() {
+  backgroundImage = loadImage('./assets/planodefundo.png')
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight)
+  database = firebase.database()
+  game = new Game()
+  game.start()
+}
+
+function draw() {
+  background(backgroundImage)
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+}
